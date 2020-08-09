97f25075ab7a3a71a5a5ee4ed07f9281f04a5eb0
Renata D'Avila <renatadavila@pm.me>
2020-08-09 19:33:26 -0300

    Update Django and dependencies

--- emaula/lessons/urls.py
+++ emaula/lessons/urls.py
@@ -1,30 +1,30 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from . import views
 
 app_name = 'lessons'
 
 urlpatterns = [
-    url(r'^text_list/$',
+    re_path(r'^text_list/$',
         views.TextListView.as_view(),
         name='text_list'),
 
     # == URL Lists ==
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^link_list/$', views.LinkListView.as_view(), name='link_list'),
-    url(r'^image_list/$', views.ImageListView.as_view(), name='image_list'),
-    url(r'^video_list/$', views.VideoListView.as_view(), name='video_list'),
-    url(r'^audio_list/$', views.AudioListView.as_view(), name='audio_list'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^link_list/$', views.LinkListView.as_view(), name='link_list'),
+    re_path(r'^image_list/$', views.ImageListView.as_view(), name='image_list'),
+    re_path(r'^video_list/$', views.VideoListView.as_view(), name='video_list'),
+    re_path(r'^audio_list/$', views.AudioListView.as_view(), name='audio_list'),
 
     # == URL details ==
-    url(r'^text/(?P<pk>[0-9]+)/$',
+    re_path(r'^text/(?P<pk>[0-9]+)/$',
         views.TextDetailView.as_view(), name='text_detail'),
-    url(r'^link/(?P<pk>[0-9]+)/$',
+    re_path(r'^link/(?P<pk>[0-9]+)/$',
         views.LinkDetailView.as_view(), name='link_detail'),
-    url(r'^image/(?P<pk>[0-9]+)/$',
+    re_path(r'^image/(?P<pk>[0-9]+)/$',
         views.ImageDetailView.as_view(), name='image_detail'),
-    url(r'^video/(?P<pk>[0-9]+)/$',
+    re_path(r'^video/(?P<pk>[0-9]+)/$',
         views.VideoDetailView.as_view(), name='video_detail'),
-    url(r'^audio/(?P<pk>[0-9]+)/$',
+    re_path(r'^audio/(?P<pk>[0-9]+)/$',
         views.AudioDetailView.as_view(), name='audio_detail'),
 ]
--- emaula/school/urls.py
+++ emaula/school/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from . import views
 
@@ -7,32 +7,32 @@ app_name = 'school'
 urlpatterns = [
     # Barra de navegação
     # url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^$', views.home_page, name='index'),
-    url(r'^about/$', views.about_project, name='about'),
-    url(r'^team/$', views.team, name='team'),
-    url(r'^colaborate/$', views.colaborate, name='colaborate'),
-    url(r'^tos/$', views.tos, name='tos'),
+    re_path(r'^$', views.home_page, name='index'),
+    re_path(r'^about/$', views.about_project, name='about'),
+    re_path(r'^team/$', views.team, name='team'),
+    re_path(r'^colaborate/$', views.colaborate, name='colaborate'),
+    re_path(r'^tos/$', views.tos, name='tos'),
 
     # Status do site
-    url(r'^sitestatus/$', views.sitestatus, name='sitestatus'),
-    url(r'^professor/$', views.ProfessorListView.as_view(), name='professor'),
+    re_path(r'^sitestatus/$', views.sitestatus, name='sitestatus'),
+    re_path(r'^professor/$', views.ProfessorListView.as_view(), name='professor'),
 
     # Lista de aulas
-    url(r'^classrooms/$',
+    re_path(r'^classrooms/$',
         views.ClassroomListView.as_view(), name='classroom_list'),
 
 
-    url(r'^professor_lessons/(?P<pk>\d+)/$',
+    re_path(r'^professor_lessons/(?P<pk>\d+)/$',
         views.professor_lessons_list, name='professor_lessons_list'),
 
-    url(r'^classroom/(?P<pk>\d+)$',
+    re_path(r'^classroom/(?P<pk>\d+)$',
         views.ClassroomDetailView.as_view(), name='classroom_detail'),
 
     # Detalhe professor
-    url(r'^professor/(?P<pk>\d+)/$',
+    re_path(r'^professor/(?P<pk>\d+)/$',
         views.ProfessorDetailView.as_view(), name='professor_detail'),
 
     # Registro professor
-    url(r'^register/$', views.register, name='register'),
+    re_path(r'^register/$', views.register, name='register'),
 
 ]
